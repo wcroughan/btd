c05ce6b15afe8c595308f6932665ea5656503659
wcroughan <croughanb@gmail.com>
2021-05-15 21:01:57 -0700

    connects to mongo

--- backend/controllers/controllers.js
+++ backend/controllers/controllers.js
@@ -1,7 +1,7 @@
 module.exports = {
     getTest(req, res, next) {
         res.status(200).json({
-            body: process.env.MONGODB_URI.substring(0, 10)
+            body: "Becca is great!!!"
         })
     }
 }
--- server.js
+++ server.js
@@ -2,42 +2,54 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+const { MongoClient } = require('mongodb');
 const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-
 const port = process.env.PORT || 5000;
-
 app.use((req, res, next) => {
     console.log(`Request_Endpoint: ${req.method} ${req.url}`);
     next();
 });
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
 app.use(cors());
 
-const apirouter = require('./backend/routes/routes');
-app.use('/api/v1/', apirouter);
+const client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    if (err) {
+        console.log("Couldn't connect to mongodb");
+        console.log(err);
+        client.close();
+    } else {
+        console.log("Connected to Mongo!");
+        const btdb = client.db("testdb");
 
-app.use(express.static(path.join(__dirname, 'client/dist')));
-// app.get('/', function (req, res) {
-//     res.sendFile(path.join(__dirname, 'client/dist/index.html'));
-// })
+        try {
+            const apirouter = require('./backend/routes/routes');
+            app.use('/api/v1/', apirouter);
 
-app.get('*', (req, res) => {
-    res.status(200).json({
-        // msg: process.env
-        msg: 'Catch all'
-    })
-})
+            app.use(express.static(path.join(__dirname, 'client/dist')));
+            app.get('*', (req, res) => {
+                res.status(200).json({
+                    // msg: process.env
+                    msg: 'Catch all'
+                })
+            })
 
-app.listen(port, () => {
-    console.log(`BACK_END_SERVICE_PORT: ${port}`);
+            app.listen(port, () => {
+                console.log(`BACK_END_SERVICE_PORT: ${port}`);
+            });
+
+        } catch (err) {
+            console.log("Error trying to set up server", err);
+            client.close();
+        }
+    }
 });
+
 
 
